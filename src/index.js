b50bd3e64cd82d6bc75b0b76d858ed2571453cb0
ct-chou <63345621+ct-chou@users.noreply.github.com>
2025-03-25 16:51:32 -0400

    Setup: html template skeleton, populate text and downloaded images. Setup basic css to create layout for mobile view

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './styles/index.css'
+
+// import ollieImage from "./ollie1.jpg";
+   
+// const image = document.createElement("img");
+// image.src = ollieImage;
+   
+// document.body.appendChild(image);
